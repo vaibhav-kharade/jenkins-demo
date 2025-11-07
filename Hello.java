2409ba81229cdbf9b1824fd6e2c8be0208db6012
Vaibhav Kharade <vaibhavkharade1000@gmail.com>
2025-11-07 13:17:16 +0530

    class name corrected

--- Hello.java
+++ Hello.java
@@ -1,6 +1,5 @@
-public class{
+public class Hello {
 	public static void main(String [] args){
-System.out.println("Hello, this is a java class for jenkins learning purpose nothing else");
-}
-
+		System.out.println("Hello, this is a java class for jenkins learning purpose nothing else");
+	}
 }
